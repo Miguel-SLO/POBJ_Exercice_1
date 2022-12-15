95515588e8e17b81c90e1f1de8d77d252019ee00
ETMLNET\migsantos <etml@etml.ch>
2022-12-15 11:25:40 +0100

    Optimisation du code + suppression des commentaires de l'ancien code C

--- POBJ_Exercice_1/POBJ_Exercice_1/Ex1.cpp
+++ POBJ_Exercice_1/POBJ_Exercice_1/Ex1.cpp
@@ -15,43 +15,32 @@ int main (void)
 	short ValB1;
 	double ValB2;
 
-	/*printf("Exercice 1 : Huber Christian \n");*/
-	/*printf("Solution en C a trandformer en C++ \n");*/
 	cout << "Exercice 1 : Huber Christian" << endl;
 	cout << "Solution en C à transformer en C++" << endl;
 
 	do {
-		/*printf("Test A ou B, Q pour Quitter \n");*/
 		cout << "Test A ou B, Q pour Quitter" << endl << flush;
-
-		/*scanf_s("%c%*c", &UserAnswer, 2);*/
 		cin >> UserAnswer;
 		
 		switch (UserAnswer) {
 			case 'A':
 			case 'a':
 
-				/*printf("TestA: entrez un nombre entre 1 et 9 \n");*/
 				cout << "TestA : entrez un nombre entre 1 et 9" << endl << flush;
-				
-				/*scanf_s("%d%*c", &ValA);*/
 				cin >> ValA;
 
 				if (ValA > 9)
 				{
 					ValA = 9;
-					/*printf("TestA: ValA limitee a 9 \n");*/
 					cout << "TestA: ValA limitee a 9 " << endl;
 				}
 				else if (ValA == 0)
 				{
 					ValA = 1;
-					/*printf("TestA: ValA forcee a 1 \n");*/
 					cout << "TestA: ValA forcee a 1" << endl;
 				}
 				else if (ValA > 0 )  
 				{
-					/*printf("%d ", ValA);*/
 					cout << ValA;
 
 					for (i=0; i < ValA ; i++)
@@ -59,17 +48,12 @@ int main (void)
 					
 					cout << endl; // saut de ligne
 				}
-				else 
-				{
-					/*printf("TestA: ValA est negatif ! \n");*/
+				else		
 					cout << "TestA: ValA est negatif !" << endl;
-				}
 				break;
 
 			case 'B':
 			case 'b':
-				//printf("TestB: entrez une valeur entre 0 et 9 \n");
-				//scanf_s("%d%*c", &ValB);
 				cout << "TestB : entrez une valeur entre 0 et 9" << endl;
 				cin >> ValB;
 				
@@ -78,15 +62,13 @@ int main (void)
 					for (i=0; i < ValB; i++) { 
 						ValB1 = 100 + (10 * i);
 						ValB2 = ValB1 / 10000.0;
-						//printf("TestB: i= %d ValB1 = %d ValB2 = %lf \n", i, ValB1, ValB2);
+
 						cout << "TestB: i = " << i << " ValB1 = " << ValB1 << " ValB2 = " << ValB2 << endl;
 					}
 				}
 				else
-				{
-					//printf("TestB: ValB n'est pas entre 0 et 9 ! \n");
 					cout << "TestB: ValB n'est pas entre 0 et 9 !" << endl;
-				}
+				
 				break;
 
 		} // end switch
