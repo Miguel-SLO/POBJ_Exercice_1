b6d91755fe83e3791b3d76ee3a9b5ba8b634f014
ETMLNET\migsantos <etml@etml.ch>
2022-12-15 11:23:36 +0100

    Changement du code C en C++ !

--- POBJ_Exercice_1/POBJ_Exercice_1/Ex1.cpp
+++ POBJ_Exercice_1/POBJ_Exercice_1/Ex1.cpp
@@ -40,44 +40,52 @@ int main (void)
 				if (ValA > 9)
 				{
 					ValA = 9;
-					printf("TestA: ValA limitee a 9 \n");
+					/*printf("TestA: ValA limitee a 9 \n");*/
+					cout << "TestA: ValA limitee a 9 " << endl;
 				}
-				if (ValA == 0)
+				else if (ValA == 0)
 				{
 					ValA = 1;
-					printf("TestA: ValA forcee a 1 \n");
+					/*printf("TestA: ValA forcee a 1 \n");*/
+					cout << "TestA: ValA forcee a 1" << endl;
 				}
-				if (ValA > 0 )  
+				else if (ValA > 0 )  
 				{
-					printf("%d ", ValA);
+					/*printf("%d ", ValA);*/
+					cout << ValA;
+
 					for (i=0; i < ValA ; i++)
-					{
-						printf ("*");
-					}
-					printf("\n"); // saut de ligne
+						cout << "*";
+					
+					cout << endl; // saut de ligne
 				}
 				else 
 				{
-					printf("TestA: ValA est negatif ! \n");
+					/*printf("TestA: ValA est negatif ! \n");*/
+					cout << "TestA: ValA est negatif !" << endl;
 				}
 				break;
 
 			case 'B':
 			case 'b':
-				printf("TestB: entrez une valeur entre 0 et 9 \n");
-				scanf_s("%d%*c", &ValB);
+				//printf("TestB: entrez une valeur entre 0 et 9 \n");
+				//scanf_s("%d%*c", &ValB);
+				cout << "TestB : entrez une valeur entre 0 et 9" << endl;
+				cin >> ValB;
 				
 				if (ValB >= 0 && ValB <=9 )
 				{
 					for (i=0; i < ValB; i++) { 
 						ValB1 = 100 + (10 * i);
 						ValB2 = ValB1 / 10000.0;
-						printf("TestB: i= %d ValB1 = %d ValB2 = %lf \n", i, ValB1, ValB2);
+						//printf("TestB: i= %d ValB1 = %d ValB2 = %lf \n", i, ValB1, ValB2);
+						cout << "TestB: i = " << i << " ValB1 = " << ValB1 << " ValB2 = " << ValB2 << endl;
 					}
 				}
 				else
 				{
-					printf("TestB: ValB n'est pas entre 0 et 9 ! \n");
+					//printf("TestB: ValB n'est pas entre 0 et 9 ! \n");
+					cout << "TestB: ValB n'est pas entre 0 et 9 !" << endl;
 				}
 				break;
 
